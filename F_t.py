f7f7033f39ecc3e4b8152b2a55803145077488d9
shaymore97 <102329973+shaymore97@users.noreply.github.com>
2022-03-24 17:01:26 -0600

    Add files via upload

---
+++ F_t.py
@@ -0,0 +1,20 @@
+import math
+def F_t(params):
+    p_0 = params[0]
+    r_w = params[1]
+    r_p = params[2]
+    r_g = params[3]
+    
+    #Assuming r_w,r_p & r_g given in meters, p_0 in psi
+    
+    #Convert p_0 to Pa
+    
+    p_0 = p_0 * 6894.76
+    
+    F_rack = p_0 *math.pi*r_p**2
+    F_t = (F_rack*r_g)/r_w
+    
+    return(F_t)
+    
+    
+    
