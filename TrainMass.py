dcd8a8ac545c162dbec5644dddcb7601552f1f22
Alexearl1 <102275433+Alexearl1@users.noreply.github.com>
2022-03-24 18:41:48 -0600

    Update TrainMass.py

--- TrainMass.py
+++ TrainMass.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Mar 24 16:23:34 2022
-
-@author: u1095484
-"""
-
 import math as m
 
 #Nathan Vance - AID2
@@ -12,7 +5,7 @@ import math as m
 #Imputs mass in kg and length in cm
 #Outputs kg
 
-def TrainMass (ID, ILength, Hardware = .5, Density = .00144):
+def TrainMass (ID, ILength, Hardware = .5, Density = 1440):
     wallTHK = ID*.06 #Approximate wall thickness to be 6% of the ID
     OD = ID + (2*wallTHK) #Assuming 
     OLength = ILength + wallTHK
@@ -23,11 +16,11 @@ def TrainMass (ID, ILength, Hardware = .5, Density = .00144):
     
     return mTrain
 
-#%% Testing
 
+"""
 #10cm Long Pipe with 10cm ID
-print(TrainMass(10,10))
+print(TrainMass(.1,.1))
 
 #Tank Mass (additional hardware mass = 0)
-print(TrainMass(10,10,0))
-
+print(TrainMass(.1,.1,0))
+"""
